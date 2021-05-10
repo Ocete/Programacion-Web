660608e73a895e2d2810d1f39b06800504a61d99
José Antonio Álvarez <joseantonioao32@gmail.com>
2021-05-10 16:48:13 +0200

    Subiendo ejercicio de form interactivo

---
+++ ejercicios/interactive form/procesar_formulario.php
@@ -0,0 +1,19 @@
+<?php
+  $campos = array('nombre',
+                  'apellido',
+                  'password1',
+                  'password2',
+                  'gustaChocolate',
+                  'noGustaChocolate',
+                  'rangoEdad',
+                  'deporte',
+                  'comentarios'),
+
+  foreach ( $campos as $campo ) {
+    if ( isset( $_POST[$campo] ) ) {
+      echo 'Campo: '.$campo.'. Valor: '.$_POST[$campo].'.';
+    } else {
+      echo 'Campo: '.$campo.' - no se introdujo';
+    }
+  }
+?>
