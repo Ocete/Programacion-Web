d3450bbfcd78c65e406d304b5e8d67f11632ff01
Ocete <ocetebt@hotmail.com>
2021-06-19 10:52:35 +0200

    Ejercicios atrasados

---
+++ ejercicios/calculadora/calculadora.php
@@ -0,0 +1,96 @@
+<!-- Ejemplo tomado del libro: Beginning PHP 5.3. Matt Doyle. Wiley Publishing, Inc. 2010 -->
+<!DOCTYPE html >
+<html>
+<head>
+<title>Overriding Methods in the Parent Class</title>
+<link rel="stylesheet" type="text/css" href="common.css" />
+</head>
+	<body>
+		<h1>Calculator Class Example</h1>
+
+		<?php
+			class Calculator {
+				protected $_val1;
+				protected $_val2;
+
+				function __construct($val1, $val2) {
+					$this->_val1 = $val1;
+					$this->_val2 = $val2;
+				}
+
+				public function setVal1($val1) {
+					$this->_val1 = $val1;
+				}
+				public function setVal2($val2) {
+					$this->_val2 = $val2;
+				}
+
+				public function sum() {
+					echo $this->_val1." + ".$this->_val2." = ";
+					echo $this->_val1+$this->_val2;
+					echo "<br>";
+				}
+				public function subtract() {
+					echo $this->_val1." - ".$this->_val2." = ";
+					echo $this->_val1-$this->_val2;
+					echo "<br>";
+				}
+				public function multiply() {
+					echo $this->_val1." * ".$this->_val2." = ".$this->_val1 * $this->_val2;
+					echo "<br>";
+				}
+				public function divide() {
+					echo $this->_val1." / ".$this->_val2." = ".$this->_val1 / $this->_val2;
+					echo "<br>";
+				}
+				public function compute() {
+					$this->sum();
+					$this->subtract();
+					$this->multiply();
+					$this->divide();
+				}
+			}
+
+			class ComplexCalculator extends Calculator {
+				function __construct($val1) {
+					parent::__construct($val1, 1);
+				}
+				public function pow() {
+					echo $this->_val1." ^ ".$this->_val2." = ";
+					echo pow($this->_val1,$this->_val2);
+					echo "<br>";
+				}
+				public function sqrt() {
+					echo "sqrt (".$this->_val1.") = ";
+					echo sqrt($this->_val1);
+					echo "<br>";
+				}
+				public function exp() {
+					echo "exp (".$this->_val1.") = ";
+					echo exp($this->_val1);
+					echo "<br>";
+				}
+				public function compute() {
+					parent::compute();
+					$this->pow();
+					$this->sqrt();
+					$this->exp();
+				}
+
+			}
+
+			echo "<h2>Calculator with values 2 and 3:</h2>";
+			$calc = new Calculator(2, 3);
+			$calc->compute();
+
+			echo "<h2>Complex Calculator with single value 5:</h2>";
+			$complex_calc = new ComplexCalculator(5);
+			$complex_calc->compute();
+
+
+			echo "<h2>Complex Calculator with values 5 and 3:</h2>";
+			$complex_calc->setVal2(3);
+			$complex_calc->compute();
+		?>
+	</body>
+</html>
---
+++ ejercicios/formulario vectorial/formulario.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html >
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="common.css" />
+</head>
+	<body>
+		<h1>Vectorial Form Example</h1>
+
+		<?php
+			class VectForm {
+				private $n_elems;
+				private $ids = array();
+				private $labels = array();
+				private $types = array();
+
+				function __construct($my_ids, $my_labels, $my_types) {
+					$this->n_elems = count($my_ids);
+					$this->ids = $my_ids;
+					$this->labels = $my_labels;
+					$this->types = $my_types;
+				}
+
+				public function print_form() {
+					echo "<form>";
+						for($i = 0; $i < $this->n_elems; $i++) {
+							echo '<label for="'.$this->ids[$i].'">'.$this->labels[$i].':</label>   ';
+							echo '<input type="'.$this->types[$i].'"id="'.$this->ids[$i].'"><br>';
+						}
+					echo '<br><input type="submit" value="Submit">';
+					echo '</form>';
+				}
+			}
+
+			$my_form = new VectForm(
+				array('username', 'password', 'color', 'age'),
+				array('Username', 'Password', 'Favorite color', 'Age'),
+				array('text', 'password', 'color', 'number'));
+
+
+			$my_form->print_form();
+		?>
+	</body>
+</html>
